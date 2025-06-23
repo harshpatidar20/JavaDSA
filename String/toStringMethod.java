5a5571fdb6d04a3df1bea00365e776b97146134c
harshpatidar20 <harshrajpatidar2001@gmail.com>
2025-06-23 12:24:48 +0530

    Add files via upload

---
+++ String/toStringMethod.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class toStringMethod
+{
+public static void main(String gg[])
+{
+int number=18;
+String str=Integer.toString(number);
+System.out.println(str.length());
+
+}
+}
